2ca578a2e2e4c2d954d96e1644db3fba46cd1624
MarcosMartinsDeLima <0timetraveler@gmail.com>
2024-08-03 15:54:46 -0300

    feat: criando hook para fazer fetch de todos os documents

---
+++ src/hooks/useFetchDocument.jsx
@@ -0,0 +1,56 @@
+import { useState,useEffect } from "react";
+import {db} from '../firebase/config'
+import { collection,query,orderBy,onSnapshot,where, QuerySnapshot } from "firebase/firestore";
+
+export const useFetchDocument = (docCollection,search = null,uid = null) => {
+    const [documents,setDocuments] = useState(null)
+    const [error,setError] = useState(null)
+    const [loading,setLoading] = useState(null)
+
+    //lidar com vazamento de memória
+    const [cancelled,setCancelled] = useState(false)
+
+    useEffect(()=>{
+        async function loadData(){
+            if(cancelled)return
+
+            setLoading(true)
+
+            const collectionRef = await collection(db,docCollection)
+
+            try {
+                let q;
+                //busca
+                ///dashboard
+
+                q = await query(collectionRef,orderBy("createdAt",'desc'))
+
+                //mapear dados, quando tiver um dado alterado ele busca 
+                await onSnapshot(q,(querySnapshot) =>{
+                    setDocuments(
+                        querySnapshot.docs.map((doc)=>({
+                            id:doc.id,
+                            ...doc.data(),
+                        }))
+                    )
+                })
+
+                setLoading(false)
+
+            } catch (error) {
+                
+                console.log(error)
+                setError(error.message)
+                setLoading(false)
+            }
+        }
+        loadData();
+    },
+    [docCollection,search,uid,cancelled])
+
+    useEffect(()=>{
+        return () =>setCancelled(true)
+    },[])
+
+    return {documents,loading,error};
+}
